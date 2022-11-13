d47ee107e2a4cc707347dc46122b9a60c6b2a5a6
Joshua Clingo <joshuaclingo@gmail.com>
2022-11-12 16:14:23 -0800

    Built out save/load system
    Copied from tutorial (TODO: adapt to specific needs here)

---
+++ Hopalong Visualizer/Assets/Scripts/DataPersistence/Data/GameData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData
+{
+    public long lastUpdated;
+    // TODO: put everything we're tracking in here
+
+    public GameData()
+    {
+
+    }
+
+
+
+
+}
